b21a97929fd20f304a3acd483806364ac713678e
Diego Papale <diegopapale@hotmail.com>
2024-08-20 03:03:01 -0300

    Commit Projeto de Testes

---
+++ ProjectB3.Tests/InvestmentServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectB3_API.Application.Services;
+using ProjectB3_API.Domain.Models;
+using System;
+
+namespace ProjectB3.Tests
+{
+    [TestClass]
+    public class InvestmentServiceTests
+    {
+        private InvestmentService _investmentService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _investmentService = new InvestmentService();
+        }
+
+        [TestMethod]
+        public void CalculateInvestment_ShouldReturnCorrectValues()
+        {
+            // Arrange
+            decimal initialValue = 1000m;
+            int months = 12;
+
+            // Act
+            var investment = _investmentService.CalculateInvestment(initialValue, months);
+
+            // Assert
+            Assert.IsTrue(investment.GrossValue > initialValue);
+            Assert.IsTrue(investment.NetValue < investment.GrossValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculateInvestment_ShouldThrowException_ForInvalidMonths()
+        {
+            // Act
+            _investmentService.CalculateInvestment(1000, 0);
+        }
+
+        [TestMethod]
+        public void CalculateInvestment_ShouldApplyCorrectTax_ForLessThan6Months()
+        {
+            // Arrange
+            decimal initialValue = 1000m;
+            int months = 6;
+
+            // Act
+            var investment = _investmentService.CalculateInvestment(initialValue, months);
+
+            // Assert
+            decimal expectedTax = 0.225m;
+            decimal grossProfit = investment.GrossValue - initialValue;
+            decimal expectedNetValue = investment.GrossValue - (grossProfit * expectedTax);
+
+            Assert.AreEqual(expectedNetValue, investment.NetValue);
+        }
+
+        [TestMethod]
+        public void CalculateInvestment_ShouldApplyCorrectTax_ForMoreThan24Months()
+        {
+            // Arrange
+            decimal initialValue = 1000m;
+            int months = 25;
+
+            // Act
+            var investment = _investmentService.CalculateInvestment(initialValue, months);
+
+            // Assert
+            decimal expectedTax = 0.15m;
+            decimal grossProfit = investment.GrossValue - initialValue;
+            decimal expectedNetValue = investment.GrossValue - (grossProfit * expectedTax);
+
+            Assert.AreEqual(expectedNetValue, investment.NetValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CalculateInvestment_ShouldThrowException_ForInvalidValues()
+        {
+            // Act
+            _investmentService.CalculateInvestment(0, 12);
+        }
+    }
+}
---
+++ ProjectB3.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectB3.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectB3.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f12ba548-c081-4a2d-b2c1-63b4637880e0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
